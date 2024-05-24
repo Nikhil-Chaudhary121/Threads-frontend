83dca1366e2a7da888103a0ac947803e48f3756f
Nikhil-Chaudhary121 <Nikhilchaudhary9887@gmail.com>
2024-05-24 14:36:38 +0530

    added backend

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://threads-backend-1-bpzb.onrender.com",
         changeOrigin: true,
         secure: false,
       },
